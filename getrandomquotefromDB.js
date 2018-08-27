6c99160994c284b6ce823ab41c374253cc65d5f6
Sunny Jaiswal <mailsunnyjk@gmail.com>
2018-08-26 19:50:10 -0500

    Removed unncessary lines

--- getrandomquotefromDB.js
+++ getrandomquotefromDB.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose');
 var Task = require('./quoteModel.js');
-var randquote = 'xyz';
 var random = require('mongoose-random');
 Quote = mongoose.model('Quotes');
-mongoose.connect('mongodb://localhost:27017/quoteGeneratorDB');//, (err, res) =>{
-
+mongoose.connect('mongodb://localhost:27017/quoteGeneratorDB');
 
 exports.random_quote = function(callback){
 	Quote.findRandom().limit(1).exec(function(err, quote){
